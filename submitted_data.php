c684be47ed5785e247de443fe81f674d3c24a461
Bertodemus <roberto@robertorubet.com>
2020-08-27 15:13:50 -0400

    spent more time getting the code cleaned up; looked into form submit

--- submitted_data.php
+++ submitted_data.php
@@ -1,18 +1,17 @@
 <?php 
-if(isset($_POST['submit'])){
-    $to = "roberto@robertorubet.com"; 
+    $to = 'roberto@robertorubet.com'; 
     $from = $_POST['userEmail']; 
     $full_name = $_POST['userName'];
     $subject = "Form submission";
     $subject2 = "Copy of your form submission";
     $message = $full_name . " wrote the following:" . "\n\n" . $_POST['userMessage'];
     $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['userMessage'];
+    $message = wordwrap($message, 70, "\r\n");
+    $message2 = wordwrap($message2, 70, "\r\n");
 
     $headers = "From:" . $from;
     $headers2 = "From:" . $to;
     mail($to,$subject,$message,$headers);
     mail($from,$subject2,$message2,$headers2);
-    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
-
-    }
+    echo "Mail Sent. Thank you " . $full_name . ", we will contact you shortly.";
 ?>
