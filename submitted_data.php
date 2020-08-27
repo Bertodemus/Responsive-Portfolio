4457a2ec7ea15d7234cc3bbdbd69296e186ffbca
Bertodemus <roberto@robertorubet.com>
2020-08-26 23:10:23 -0400

    minor adjustments to style.css and added in email icon

--- submitted_data.php
+++ submitted_data.php
@@ -1,7 +1,7 @@
 <?php 
 if(isset($_POST['submit'])){
-    $to = "roberto@robertorubet.com"; // this is your Email address
-    $from = $_POST['userEmail']; // this is the sender's Email address
+    $to = "roberto@robertorubet.com"; 
+    $from = $_POST['userEmail']; 
     $full_name = $_POST['userName'];
     $subject = "Form submission";
     $subject2 = "Copy of your form submission";
@@ -11,9 +11,8 @@ if(isset($_POST['submit'])){
     $headers = "From:" . $from;
     $headers2 = "From:" . $to;
     mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    mail($from,$subject2,$message2,$headers2);
     echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
-    // You cannot use header and echo together. It's one or the other.
+
     }
 ?>
